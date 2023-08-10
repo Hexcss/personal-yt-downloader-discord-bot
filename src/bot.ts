d13d028a88733273a98bd90575ece2859b2dd38e
Hexcss <cadersuay.javier@gmail.com>
2023-08-10 21:15:33 +0200

    feat: better message

--- src/bot.ts
+++ src/bot.ts
@@ -1,33 +1,13 @@
-import { Client, GatewayIntentBits } from 'discord.js';
-import axios from 'axios';
-import { environment } from './config/environment';
+import { Client, GatewayIntentBits } from "discord.js";
+import { environment } from "./config/environment";
+import { handleInteractionCreate } from "./events/interactionCreate";
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const token = environment.discordBotToken; // Replace with your bot token
-
-client.once('ready', () => {
-    console.log('Bot is ready!');
+client.once("ready", () => {
+  console.log("Bot is ready!");
 });
 
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return;
+client.on("interactionCreate", handleInteractionCreate);
 
-    const { commandName } = interaction;
-
-    if (commandName === 'download') {
-      const url = (interaction.options as any).getString('url');
-
-        // Send a request to your API
-        try {
-            const response = await axios.post((environment.youtubeApiEndpoint as string), { url: url });
-            const downloadLink = response.data; // Assuming your API returns a direct link
-
-            await interaction.reply(`Here's your download link: ${downloadLink}`);
-        } catch (error) {
-            await interaction.reply('There was an error processing your request.');
-        }
-    }
-});
-
-client.login(token as string);
+client.login(environment.discordBotToken);
