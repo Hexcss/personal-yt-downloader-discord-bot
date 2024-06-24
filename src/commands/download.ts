ee820de112fe74fc4a45da149cbe382fea2382a5
javier.cader <javier.cader@ct.uneatlantico.es>
2024-06-25 01:59:27 +0200

    feat: Shorten URL

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -3,13 +3,17 @@ import axios from "axios";
 import { environment } from "../config/environment";
 import { createDownloadEmbed } from '../utils/embedBuilder';
 
+async function shortenUrl(longUrl: string): Promise<string> {
+  const response = await axios.get(`http://tinyurl.com/api-create.php?url=${encodeURIComponent(longUrl)}`);
+  return response.data;
+}
+
 export async function handleDownloadCommand(interaction: CommandInteraction) {
   const url = (interaction.options as any).getString("url");
 
   await interaction.deferReply();
 
   try {
-
     console.log("Requesting download for:", url);
 
     const response = await axios.post((environment.youtubeApiEndpoint as string) + "/ytdl/downloadmp3", { url: url });
@@ -17,26 +21,26 @@ export async function handleDownloadCommand(interaction: CommandInteraction) {
     console.log("Response Status:", response.status);
 
     if (typeof response.data === "string") {
-      const { embeds, components }= createDownloadEmbed(response.data);
-      await interaction.editReply({ embeds, components } as { embeds: EmbedBuilder[]});
+      const shortUrl = await shortenUrl(response.data); // Shorten the URL here
+      const { embeds, components } = createDownloadEmbed(shortUrl);
+      await interaction.editReply({ embeds, components } as { embeds: EmbedBuilder[] });
     } else {
       console.error("Unexpected response format:", response.data);
       await interaction.editReply("There was an error processing your request. Unexpected response format.");
     }
   } catch (error: any) {
     console.error("Error:", error.message);
-    console.error(error)
+    console.error(error);
     if (error.response) {
       console.error("Response Status:", error.response.status);
       console.error("Response Data:", error.response.data);
       if (error.response.status === 400) {
-          await interaction.editReply("The video took too long to convert.");
+        await interaction.editReply("The video took too long to convert.");
       } else {
-          await interaction.editReply("There was an error processing your request.");
+        await interaction.editReply("There was an error processing your request.");
       }
     } else {
       await interaction.editReply("There was an error processing your request.");
     }
   }
 }
-
